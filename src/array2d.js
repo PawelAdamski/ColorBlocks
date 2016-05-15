7dae59135a32d4f82e3b71370530c340e172d7cb
Paweł Adamski <pawel@agflow.com>
2016-05-15 22:16:38 +0200

    Init commit

---
+++ src/array2d.js
@@ -0,0 +1,21 @@
+
+
+function array2d(d1){
+
+	this.log = [];
+	this.tab = [];
+
+	for (var i=0;i<d1;i++) {
+ 
+		props = createProps(i,this.log,this.tab);
+
+		Object.defineProperty(this,i,props);
+	}
+}
+
+function createProps(n,log,tab) {
+	return {
+			  get: function() { return tab[n]},
+			  set: function(newValue) { log.push({index:n,value:newValue});tab[n]=newValue;},
+	}
+}
